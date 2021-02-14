6291a1effede4b17f0b2f5660eff1eb5a75be3ca
Ilya Vlasov <ilyavlasov93@gmail.com>
2021-02-14 12:20:28 -0500

    Initial commit

---
+++ functions.js
@@ -0,0 +1,4 @@
+// Funvtion to create random number
+function random(number) {
+  return Math.ceil(Math.random() * number);
+}
---
+++ index.js
@@ -0,0 +1,32 @@
+//Call for window to ask a name
+let name = prompt('Hello, what is your name?');
+
+//Call for window untill player enter the name
+while (name == '' || name == null) {
+  name = prompt('Please, enter your name!');
+}
+
+// call for rules of the game after name were entered
+alert(name + ', I thought of a number from 1 to 100. Try to guess it off in the fewest possible attempts. After each try, I will say "little", "a lot", or "guessed".');
+
+//Call for number 1-100
+let number = random(100)
+
+// Call for window to input the guess
+let guess = prompt('What number i thinking abut?');
+let numberOfGuesses = 1;
+
+
+// Call for window untill number is guessed 
+while (guess != number) {
+  if (guess > number) {
+    guess = prompt('A lot, try again');
+    numberOfGuesses = numberOfGuesses + 1;
+  } if (guess < number) {
+    guess = prompt('A little, try again');
+    numberOfGuesses = numberOfGuesses + 1;
+  }
+}
+
+// Number of guesse after right guess 
+alert('You are right! The number is ' + number + ' You guessed it with ' + numberOfGuesses + ' tries');
